4b30059cea406da843f1dbe70b881a9c7176e7f4
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-06-14 19:54:43 +0300

    interval-change-body-color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+    startBtn : document.querySelector('button[data-start]'),
+    stopBtn : document.querySelector('button[data-stop]'),
+    bodyColor : document.querySelector('body'),
+}
 
+refs.startBtn.addEventListener('click', HandlerStart);
+refs.stopBtn.addEventListener('click', HandlerStop);
+
+let timerId = null;
+
+function HandlerStart() {    
+    timerId = setInterval(() => { getRandomHexColor() }, 1000);
+    refs.startBtn.disabled = true ;   
+};
+function HandlerStop() {
+    clearInterval(timerId);
+    refs.bodyColor.style.backgroundColor = '#fafafa';
+    refs.startBtn.disabled = false ; 
+};
+ 
+
+function getRandomHexColor() {
+    return refs.bodyColor.style.backgroundColor =
+          `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
