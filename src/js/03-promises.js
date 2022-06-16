e83224380f1bd45d85d8120de9f217075e735c4c
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-06-16 18:45:01 +0300

    tipeof error in promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// import { Report } from 'notiflix/build/notiflix-report-aio';
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount }
+  } = event.currentTarget;
+ 
+  const arg = {
+    delay: delay.value,
+    step: step.value,
+    amount:amount.value,
+  };    
+  
+  event.currentTarget.reset();  
+  
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+
+        const obj = { position: position, delay: delay };
+        
+        if (shouldResolve) {          
+          resolve(obj)
+        } else {
+          reject(obj)
+        }
+      }, delay);
+    });
+  };    
+    console.log()
+    for (let i = 0; i < arg.amount; i++) {
+      let position = i + 1;
+    let delay = arg.delay + arg.step * i;   
+    // let delay = Number(arg.delay) + Number(arg.step) * i;            
+      console.log(arg.delay)
+      console.log(arg.step)
+      console.log(arg.amount) 
+      console.log(delay)
+  createPromise(position, delay).then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+};
+};
